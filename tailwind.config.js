f5cda8a1d74716edf33937ccd3e6d5c6b9e35362
shash-camp <shashank221405@gmail.com>
2024-04-26 15:15:22 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      'primary':'#ee0181'
+    },
+  },
+  plugins: [],
+}
+
